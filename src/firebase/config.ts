f12fbade3281f94cb0089de111ff3e0e454cf285
Keyla Hehehe <keylahehehe42@gmail.com>
2026-01-26 15:22:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8188506987-578df",
+  "appId": "1:369042896543:web:cb8f0e603eba904c8554aa",
+  "apiKey": "AIzaSyC2Jb0bai-AMB25yU1AbdUNoi4Isb-AGvY",
+  "authDomain": "studio-8188506987-578df.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "369042896543"
+};
